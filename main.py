bf680e5933919c2750712b84bd0c91eb46a6b46d
Ramon Canizares <juniorcqusa@gmail.com>
2024-04-27 03:07:48 -0400

    set up for aws

--- main.py
+++ main.py
@@ -10,5 +10,5 @@ async def read_root():
     return {"message": "Hello, World!"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
 
